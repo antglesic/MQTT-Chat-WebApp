c63ba8098294f9b74f1b273fc46745f54e35c104
Antonio Glešić <aglesic@gmail.com>
2018-07-26 15:43:11 +0200

    Sitne izmjene i ispravci, dodani komentari za javascript dio...

--- js/antglesic.js
+++ js/antglesic.js
@@ -1,11 +1,37 @@
 
-// Create a client instance
+// Kreira se instanca klijenta koja je zapravo NULL
 client = null;
 connected = false;
 
-// called when the client connects
+// Funkcija koja se poziva pritiskom na dugme za povezivanje
+function connect(){
+    var hostname = 'broker.mqttdashboard.com'; //Unaprijed određeni poslužitelj (broker)
+    var port = '8000'; //port
+    var clientId = document.getElementById("ime").value; //Dohvaćanje imena korisnika
+  
+    client = new Paho.MQTT.Client(hostname, Number(port), clientId); //Kreiranje instance MQTT klijenta
+    
+    console.info('Connecting to Server: Hostname: ', hostname, '. Port: ', port, '. Client ID: ', clientId);
+
+    // Postavljanje metoda koje će se koristiti kao event handleri za određene događaje
+    client.onConnectionLost = onConnectionLost;
+    client.onMessageArrived = onMessageArrived;
+
+    // Parametri koje funkcija za uspostavljanje veze poprima spremljeni u jednu varijablu
+    var options = {
+      invocationContext: {host : hostname, port: port, clientId: clientId},
+      onSuccess: onConnect,
+      onFailure: onFail
+    };
+    
+    // Povezivanje klijenta
+    client.connect(options);
+
+}
+
+
+// Event handler za uspješno povezivanje
 function onConnect(context) {
-  // Once a connection has been made, make a subscription and send a message.
   console.log("Client Connected");
   connected = true;
   var poruka = document.createElement('span');
@@ -16,6 +42,7 @@ function onConnect(context) {
   setFormEnabledState(true);
 }
 
+// Event handler za neuspješno povezivanje
 function onFail(context) {
   console.log("Failed to connect");
   connected = false;
@@ -26,7 +53,7 @@ function onFail(context) {
   setFormEnabledState(false);
 }
 
-// called when the client loses its connection
+// Event handler za gubitak veze
 function onConnectionLost(responseObject) {
   if (responseObject.errorCode !== 0) {
     console.log("Connection Lost: " + responseObject.errorMessage);
@@ -38,16 +65,17 @@ function onConnectionLost(responseObject) {
   connected = false;
 }
 
-// called when a message arrives
+// Event handler za pristiglu poruku
 function onMessageArrived(message) {
   console.log('Message Recieved: Topic: ', message.destinationName, '. Payload: ', message.payloadString, '. QoS: ', message.qos);
   console.log(message);
-  var messageTime = new Date().toISOString();
-  var poruka = document.createElement('span');
-  poruka.innerHTML = 'Tema: ' + message.destinationName + '  |  ' + message.payloadString + '</span><br/>';
-  var messages = document.getElementById("messages");
-  messages.appendChild(poruka);
+  var messageTime = new Date().toISOString(); //timestamp primitka pristigle poruke
+  var poruka = document.createElement('span'); //kreiranje html elementa sa sadržajem poruke
+  poruka.innerHTML = 'Tema: ' + message.destinationName + '  |  ' + message.payloadString + '</span><br/>'; 
+  var messages = document.getElementById("messages"); //dohvaćanje html elementa za prikazivanje poruka
+  messages.appendChild(poruka); //dodavanje nove poruke html elementu predviđenom za prikaz pristiglih poruka
 }
+
 
 function connectionToggle(){
   if(connected){
@@ -57,31 +85,7 @@ function connectionToggle(){
   }
 }
 
-function connect(){
-    var hostname = 'broker.mqttdashboard.com';
-    var port = '8000';
-    var clientId = document.getElementById("ime").value;
-  
-    client = new Paho.MQTT.Client(hostname, Number(port), clientId);
-    
-    console.info('Connecting to Server: Hostname: ', hostname, '. Port: ', port, '. Client ID: ', clientId);
-
-    // set callback handlers
-    client.onConnectionLost = onConnectionLost;
-    client.onMessageArrived = onMessageArrived;
-
-
-    var options = {
-      invocationContext: {host : hostname, port: port, clientId: clientId},
-      onSuccess: onConnect,
-      onFailure: onFail
-    };
-    
-    // connect the client
-    client.connect(options);
-
-}
-
+// Metoda za ugasit vezu klijenta s poslužiteljem
 function disconnect(){
     console.info('Disconnecting from Server');
     client.disconnect();
@@ -93,10 +97,10 @@ function disconnect(){
     setFormEnabledState(false);
 }
 
-// Sets various form controls to either enabled or disabled
+// Postavlja html elemente da budu omogućeni ili neomogućeni
 function setFormEnabledState(enabled){
 
-    // Connection Panel Elements
+    // Elementi za povezivanje ili gašenje veze
     if(enabled){
       document.getElementById("clientConnectButton").innerHTML = "Disconnect";
     } else {
@@ -109,31 +113,32 @@ function setFormEnabledState(enabled){
     document.getElementById("pretplata").disabled = !enabled;
 }
 
+// Metoda za objavljivanje poruke
 function publish(){
-    var topic = document.getElementById("tema").value;
-    var topic1 = 'zavrsni_rad/' + topic + '/test';
-    var korisnik = document.getElementById("ime").value;
-    var qos = 2;
-    var message = document.getElementById("ulaz").value;
-    var poruka = korisnik + ':  ' + message;
+    var topic = document.getElementById("tema").value; //dohvaćanje naziva teme
+    var topic1 = 'zavrsni_rad/' + topic + '/test'; //dodavanje prefiksa i sufiksa
+    var korisnik = document.getElementById("ime").value; //dohvaćanje imena korisnika
+    var qos = 2; //razina qos
+    var message = document.getElementById("ulaz").value; //sadržaj poruke
+    var poruka = korisnik + ':  ' + message; //poruka pripremljena za objavljivanje
     console.info('Publishing Message: Topic: ', topic1, '. QoS: ' + qos + '. Message: ', poruka);
-    message = new Paho.MQTT.Message(poruka);
-    message.destinationName = topic1;
-    message.qos = Number(qos);
-    client.send(message);
+    message = new Paho.MQTT.Message(poruka); //mqtt objekt poruke
+    message.destinationName = topic1; //objektu dodana destinacija
+    message.qos = Number(qos); //objektu određen qos
+    client.send(message); //objavljivanje poruke
 }
 
-
+// Metoda za pretplaćivanje na tematsku skupinu
 function subscribe(){
-    var topic = document.getElementById("tema").value;
-    var topic1 = 'zavrsni_rad/' + topic + '/test';
-    var qos = 2;
+    var topic = document.getElementById("tema").value; //dohvaćanje naziva teme
+    var topic1 = 'zavrsni_rad/' + topic + '/test'; //dodavanje prefiksa i sufiksa
+    var qos = 2; //određivanje qos
     console.info('Subscribing to: Topic: ', topic1, '. QoS: ', qos);
-    var poruka = document.createElement('span');
-    var korisnik = document.getElementById("ime").value;
+    var poruka = document.createElement('span'); 
+    var korisnik = document.getElementById("ime").value; //dohvaćanje imena korisnika
     poruka.innerHTML = korisnik + ' se pretplatio na ' + topic1 + '</span><br/>';
     var messages = document.getElementById("messages");
-    messages.appendChild(poruka);
-    client.subscribe(topic1, {qos: Number(qos)});
+    messages.appendChild(poruka); //prikazivanje poruke o pretplaćivanju
+    client.subscribe(topic1, {qos: Number(qos)}); //pretplaćivanje na tematsku skupinu
 }
 
